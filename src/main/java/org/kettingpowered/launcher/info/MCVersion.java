ad30e18244fe4b2e78daf37a34ef6bcec0f124d9
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-01-06 18:01:33 +0100

    also detect mc version from existing ketting server files

--- src/main/java/org/kettingpowered/launcher/info/MCVersion.java
+++ src/main/java/org/kettingpowered/launcher/info/MCVersion.java
@@ -1,14 +1,20 @@
 package org.kettingpowered.launcher.info;
 
 import org.kettingpowered.ketting.internal.KettingFiles;
+import org.kettingpowered.ketting.internal.MajorMinorPatchVersion;
+import org.kettingpowered.ketting.internal.Tuple;
 import org.kettingpowered.launcher.ParsedArgs;
+import org.kettingpowered.launcher.utils.FileUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -30,9 +36,21 @@ public class MCVersion {
         }
         //Get the active ketting forge dir
         {
-//            if (KettingFiles.FORGE_UNIVERSAL_JAR.exists()) {
-//                new JarFile(KettingFiles.FORGE_UNIVERSAL_JAR)
-//            }
+            List<String> versions = KettingFiles.getKettingServerVersions();
+            HashMap<MajorMinorPatchVersion<Integer>, List<Tuple<MajorMinorPatchVersion<Integer>, MajorMinorPatchVersion<Integer>>>> map = MajorMinorPatchVersion.parseKettingServerVersionList(versions.stream());
+            List<MajorMinorPatchVersion<Integer>> mc_versions = new ArrayList<>(map.keySet().stream().toList());
+            mc_versions.sort(Comparator.comparing((MajorMinorPatchVersion<Integer> t) -> t).reversed()); //sort, so that index 0 is the largest minecraft version
+            if (!mc_versions.isEmpty() && !map.get(mc_versions.get(0)).isEmpty()) {
+                final MajorMinorPatchVersion<Integer> mc_version = mc_versions.get(0);
+                map.get(mc_version).sort(Comparator.comparing((Tuple<MajorMinorPatchVersion<Integer>, MajorMinorPatchVersion<Integer>> t) -> t.t1()).thenComparing(Tuple::t2).reversed());
+                final Tuple<MajorMinorPatchVersion<Integer>, MajorMinorPatchVersion<Integer>> forge_ketting_version = map.get(mc_version).get(0);
+                try{
+                    Arrays.stream(Objects.requireNonNullElse(KettingFiles.KETTINGSERVER_FORGE_DIR.listFiles(File::isDirectory), new File[0]))
+                            .filter(file-> !String.format("%s-%s-%s", mc_version, forge_ketting_version.t1(), forge_ketting_version.t2()).equals(file.getName()))
+                            .forEach(FileUtils::deleteDir);
+                }catch (Throwable ignored){}
+                return mc_versions.get(0).toString();
+            }
         }
         //Get the last saved mc version
         {
