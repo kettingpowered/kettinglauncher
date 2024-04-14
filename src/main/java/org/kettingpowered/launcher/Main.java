acfe79547d491223f08461679b054d6e3d3c534c
TonimatasDEV <amatascubero@gmail.com>
2024-04-14 14:23:33 +0200

    Add MC version for get the path correctly

--- src/main/java/org/kettingpowered/launcher/Main.java
+++ src/main/java/org/kettingpowered/launcher/Main.java
@@ -261,7 +261,7 @@ public class Main {
                             "--launchTarget",
                             args.launchTarget()!=null? args.launchTarget() : "forgeserver",
                             "--fml.neoForgeVersion",
-                            KettingConstants.FORGE_VERSION+"-"+KettingConstants.KETTING_VERSION,
+                            KettingConstants.MINECRAFT_VERSION+"-"+KettingConstants.FORGE_VERSION+"-"+KettingConstants.KETTING_VERSION,
                             "--fml.fmlVersion",
                             fmlVersion,
                             "--fml.mcVersion",
